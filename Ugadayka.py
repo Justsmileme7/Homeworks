505f3a2421fd854af45386893b161d9a8dc3e641
evgenia <smallbee_@mail.ru>
2023-11-28 19:05:43 +0300

    refacturing

--- Ugadayka.py
+++ Ugadayka.py
@@ -1,6 +1,8 @@
 from random import randint
-def function ():
-    def function_2():
+
+
+def function_set_game():
+    def receive_range():
         while True:
             try:
                 number_from = input('Enter number from\n')
@@ -9,35 +11,36 @@ def function ():
                 int(number_to)
             except ValueError:
                 print('Error: you enter not numbers, TRY AGAIN')
-                function_2()
-            if int(number_to) - int(number_from) >= 5 and int(number_to) - int(number_from) <= 30:
-                a1 = randint((int(number_from)),(int(number_to)))
-                a2 = randint((int(number_from)),(int(number_to)))
-                a3 = randint((int(number_from)),(int(number_to)))
+                receive_range()
+            if 30 >= int(number_to) - int(number_from) >= 5:
+                a1 = randint((int(number_from)), (int(number_to)))
+                a2 = randint((int(number_from)), (int(number_to)))
+                a3 = randint((int(number_from)), (int(number_to)))
                 list_random_numbers = [a1, a2, a3]
                 set1 = set(list_random_numbers)
-                while len(set1) <3:
-                    set1.add(randint((int(number_from)),(int(number_to))))
+                while len(set1) < 3:
+                    set1.add(randint((int(number_from)), (int(number_to))))
                 print(a1, a2, a3)
                 print(set1)
             else:
                 print('Try again and Enter range from 5 to 30 numbers')
                 continue
             return set1
-    list = function_2()
 
-    def function_4():
+    list_of_range = receive_range()
+
+    def repit_function_until_win_or_exit():
         while True:
-            def function_3():
+            def function_take_numbers_from_users():
                 while True:
                     try:
-                        x = input('Введите число 1\n')
+                        x = input('Enter number 1\n')
                         if x == 'exit':
                             quit()
-                        y = input('Введите число 2\n')
+                        y = input('Enter number 2\n')
                         if y == 'exit':
                             quit()
-                        z = input('Введите число 3\n')
+                        z = input('Enter number 3\n')
                         if z == 'exit':
                             quit()
                         int(x)
@@ -47,22 +50,26 @@ def function ():
                         print('Error: you enter not numbers')
                     else:
                         break
-                if x!=y and x!=z and y!=z:
-                    list_numbers = [x,y,z]
-                    print(x,y,z)
+                if x != y and x != z and y != z:
+                    list_numbers = [x, y, z]
+                    print(x, y, z)
                     return list_numbers
                 else:
-                    print('Try again and enter diferent numbers')
-                    return function_3()
+                    print('Try again and enter different numbers')
+                    return function_take_numbers_from_users()
+
             count = 0
-            for i in function_3():
-                if int(i) in list:
-                    count = count+1
+            for i in function_take_numbers_from_users():
+                if int(i) in list_of_range:
+                    count = count + 1
                 if count == 3:
                     print('You win!')
                     exit()
             else:
                 print('Try again')
             print("You guessed", count, 'numbers')
-    function_4()
-function ()
+
+    repit_function_until_win_or_exit()
+
+
+function_set_game()
