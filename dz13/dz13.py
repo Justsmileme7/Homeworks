88718319b88b5deaff883fbc9b5136288bac24ff
evgenia <smallbee_@mail.ru>
2023-12-07 18:14:16 +0300

    dz 13 - last class works wrong

--- dz13/dz13.py
+++ dz13/dz13.py
@@ -31,7 +31,34 @@ class DataStorage:
         print('File close')
 
 
+class DataStorageWrite(DataStorage):
+    def connect(self):
+        try:
+            self.new_file = open(f'{self.path}', 'r')
+            new_content = json.load(self.new_file)
+            self.content = new_content
+        except FileNotFoundError:
+            self._create_storage()
+            self.connect()
+
+
+    def _create_storage(self):
+        with open(f'{self.path}', 'w') as new_file:
+            json.dump([], new_file)
+            return new_file
+
+    def append_new(self, line):
+        self.content = self.new_file.append(line)
+
+    def disconnect(self):
+        self.content
+        self.new_file.close()
+        print(f'{self.content} and file close')
+
+
 storage_one = DataStorage('new_file_one.json')
 storage_one.connect()
 storage_one.content
 storage_one.disconnect()
+storage_new = DataStorageWrite(DataStorage)
+storage_new.append_new('добавление')
